9348b0e9b9420ef073c9ba1c5e5ba275b55067f1
Tamsir bah <Bahtamsir91@gmail.com>
2024-12-12 22:07:46 +0100

    oui

---
+++ hooks/use-toast.ts
@@ -0,0 +1,13 @@
+"use client";
+
+import { toast } from "sonner";
+
+export function useToast() {
+  return {
+    toast: (title: string, description?: string) => {
+      toast(title, {
+        description,
+      });
+    },
+  };
+}
